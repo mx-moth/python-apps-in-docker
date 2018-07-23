423529245cbee76a61caae8958828308a0775f67
Tim Heap <tim@timheap.me>
2018-07-23 18:06:47 +1000

    Copy demo/basic/ to demo/celery/

---
+++ demo/celery/deploy/settings.py
@@ -0,0 +1,15 @@
+import os
+
+from dj_database_url import parse
+from mywebapp.settings import *  # noqa
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+DEBUG = bool(int(os.environ['DJANGO_DEBUG']))
+
+ALLOWED_HOSTS = [os.environ['DJANGO_HOSTNAME']]
+
+DATABASES = {'default': parse(os.environ['DATABASE_URL'])}
+
+ADMINS = ['errors@timheap.me']
+
+STATIC_ROOT = os.environ['DJANGO_STATIC_ROOT']
---
+++ demo/celery/develop/settings.py
@@ -0,0 +1,16 @@
+from dj_database_url import parse
+from mywebapp.settings import *
+
+SECRET_KEY = 'super secret shhhhh'
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '*']
+
+DATABASES = {'default': parse('postgres://postgres@database/postgres')}
+
+DATA_ROOT = '/opt/my-web-app/data/'
+MEDIA_ROOT = DATA_ROOT + 'media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail'
+EMAIL_PORT = 25
