a0ae6fe75eaca81e4fa7baf61c4cd9540886cba0
Tim Heap <tim@timheap.me>
2018-07-23 18:47:23 +1000

    Simplify some demo configurations

--- demo/frontend-build/develop/settings.py
+++ demo/frontend-build/develop/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from dj_database_url import parse
 from mywebapp.settings import *
 
@@ -6,11 +8,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '*']
 
-DATABASES = {'default': parse('postgres://postgres@database/postgres')}
+DATABASES = {'default': parse(os.environ['DATABASE_URL'])}
 
 DATA_ROOT = '/opt/my-web-app/data/'
 MEDIA_ROOT = DATA_ROOT + 'media/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail'
-EMAIL_PORT = 25
